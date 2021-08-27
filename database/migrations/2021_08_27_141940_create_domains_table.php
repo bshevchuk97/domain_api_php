dee1d2f253de53f20987dc7c5a742e6cbdcf1a7f
Bohdan Shevchuk <b.shevchuk@keepsolid.com>
2021-08-27 17:31:28 +0300

    added database migration for entities ApiUser, Domain and Status

---
+++ database/migrations/2021_08_27_141940_create_domains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('status_id');
+            $table->foreign('status_id')->references('id')->on('statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domains');
+    }
+}
