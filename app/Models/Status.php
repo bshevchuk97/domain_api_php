da65c179067c7018fe80fd7db644996ff0c56f1b
Bohdan Shevchuk <b.shevchuk@keepsolid.com>
2021-08-31 13:37:44 +0300

    added proper PHPDoc and table reference to Status.php, disabled timestamp generation

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Status
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Database\Factories\StatusFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Status newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Status newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Status query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Status whereName($value)
+ * @mixin \Eloquent
+ */
 class Status extends Model
 {
+    protected $table = 'statuses';
+    public $timestamps = false;
     use HasFactory;
 }
