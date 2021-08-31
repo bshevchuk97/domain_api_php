417ff31010364291835a5db403bf8d274354390e
Bohdan Shevchuk <b.shevchuk@keepsolid.com>
2021-08-31 15:04:12 +0300

    added feature of generating new session on user creation

--- app/Http/Controllers/ApiUserController.php
+++ app/Http/Controllers/ApiUserController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\Helpers\BaseApiException;
 use App\Models\Session;
+use Faker\Provider\Base;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\ApiUser;
 use Illuminate\Http\Response;
 use phpDocumentor\Reflection\Types\Integer;
+use App\Exceptions\Helpers;
+use App\Exceptions\Helpers\Concrete;
 
 class ApiUserController extends Controller
 {
 
-    private static function correctUserCredentials($username, $password_hash): bool
-    {
-        return $username != NULL && $password_hash != NULL;
-    }
+
 
     /**
      * Show the form for creating a new resource.
@@ -31,23 +33,19 @@ class ApiUserController extends Controller
 
         if (!self::correctUserCredentials($username, $password_hash))
             return \Response::json(['error' => 'You must specify both username and password hash'], 401);
-
         $user = ApiUser::where('username', '=', $username)->first();
 
         if ($user != NULL)
             return \Response::json(['error' => 'User already exists!'], 401);
 
-        $new_user = new ApiUser();
-        $new_user->username = $username;
-        $new_user->password_hash = $password_hash;
-
         try {
-            $new_user->save();
+            $new_user = $this->createUser($username, $password_hash);
+            $session = $this->createSession($new_user);
+
+            return \Response::json(['session_token' => $session->token]);
         } catch (\Throwable $e) {
             return \Response::json(['message' => $e->getMessage()], 401);
         }
-
-        return \Response::json($username);
     }
 
 
@@ -57,54 +55,73 @@ class ApiUserController extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    public function show(Request $request)
+    public function show(Request $request): JsonResponse
     {
         $username = $request->get("username");
         $password_hash = $request->get("password_hash");
 
         if (!self::correctUserCredentials($username, $password_hash))
             return \Response::json(['error' => 'You must specify both username and password hash'], 401);
-
         $user = ApiUser::where('username', '=', $username)->first();
 
         if ($user == NULL)
             return \Response::json(['error' => 'User does not exist!'], 401);
 
-        srand(time());
-        $session = new Session();
-        $session->token = md5(strval(mt_rand()) . $username);
-        $session->created = new \dateTime();
-        $session->user()->associate($user);
-
         try {
-            $session->save();
+            $session = $this->createSession($user);
+            return \Response::json(['session_token' => $session->token]);
         } catch (\Throwable $e) {
             return \Response::json(['message' => $e->getMessage()], 401);
         }
-
-        return \Response::json(['session_token' => $session->token]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
+//      /**
+//     * Update the specified resource in storage.
+//     *
+//     * @param Request $request
+//     * @param int $id
+//     * @return Response
+//     */
+//    public function update(Request $request, $id)
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Remove the specified resource from storage.
+//     *
+//     * @param int $id
+//     * @return Response
+//     */
+//    public function destroy($id)
+//    {
+//        //
+//    }
+
+    private static function correctUserCredentials($username, $password_hash): bool
     {
-        //
+        return $username != NULL && $password_hash != NULL;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
+    private function createUser($username, $password_hash): ApiUser
     {
-        //
+        $new_user = new ApiUser();
+        $new_user->username = $username;
+        $new_user->password_hash = $password_hash;
+
+        $new_user->save();
+        return $new_user;
+    }
+
+    private function createSession($user): Session
+    {
+        srand(time());
+        $session = new Session();
+        $session->token = md5(strval(mt_rand()) . $user->username);
+        $session->created = new \dateTime();
+        $session->user()->associate($user);
+
+        $session->save();
+        return $session;
     }
 }
