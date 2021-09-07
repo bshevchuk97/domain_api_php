7e1ab41313c7813765a791315acdba61d0009a9e
Bohdan Shevchuk <b.shevchuk@keepsolid.com>
2021-09-07 17:24:54 +0300

    added automatic exception forwarding to response as JSON

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use Egulias\EmailValidator\Validation\Exception\EmptyValidationList;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Container\EntryNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Phalcon\Exception;
+use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
 use Throwable;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
@@ -56,11 +60,29 @@ class Handler extends ExceptionHandler
         }
 
         return parent::render($request, $e);*/
-        if ($e instanceof ModelNotFoundException && $request->wantsJson()) {
-            return response()->json(['message' => 'Not Found!'], 404);
+        if ($e instanceof ModelNotFoundException/* && $request->wantsJson()*/) {
+            return response()->json(['error' => 'Not Found!'], 404);
         }
+        elseif ($e instanceof SessionNotFoundException) {
+            return response()->json(['error' => 'Session is either nonexistent or expired'], 401);
+        }
+        elseif ($e instanceof AuthorizationException) {
+            return response()->json(['error' => 'Session is either nonexistent or expired'], 401);
+        }
+        elseif ($e instanceof  EmptyValidationList){
+            return response()->json(['error' => 'You must specify both username and password hash'], 401);
+        }
+        elseif ($e instanceof  UserNotFoundException){
+            return response()->json(['error' => 'Specified user not found'], 401);
+        }
+        elseif ($e instanceof UserAlreadyExistsException){
+            return response()->json(['error' => 'User with such name already exists'], 401);
+        }
+        else
+            return response()->json(['error' => $e->getMessage()], 500);
 
-        return parent::render($request, $e);
+
+        /*return parent::render($request, $e);*/
     }
 
 }
