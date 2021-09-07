f676fd0fc85a0a68cc8a5fa5790e8282b8b4b71e
Bohdan Shevchuk <b.shevchuk@keepsolid.com>
2021-09-07 17:22:36 +0300

    registered DomainServiceProvider in app.php prividers section

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\DomainServiceProvider::class,
+
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
     ],
 
