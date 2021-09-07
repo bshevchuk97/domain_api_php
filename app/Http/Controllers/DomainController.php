4d7aec9aa3442e7f55bf18c0b6c977ce57a143d0
Bohdan Shevchuk <b.shevchuk@keepsolid.com>
2021-09-07 17:20:46 +0300

    moved majority of domain logic to ValidateSession middleware and DomainService class

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -2,83 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Helpers\JsonFormatHelper;
+use App\Http\Middleware\SessionProvider;
+use App\Service\Domain\DomainService;
+use App\Service\Session\SessionValidator;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class DomainController extends Controller
 {
+    private DomainService $domainService;
+
+
+    public function __construct(DomainService $domainService){
+$this->domainService = $domainService;
+    }
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        $domains = $this->domainService->getAll();
+
+        return \Response::json($domains);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        return \Response::json(
+            $this->domainService->create(
+                $request->get('name')
+            )
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param string $name
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show(int $id): JsonResponse
     {
-        //
-    }
+        $domain = $this->domainService->getById($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        if($domain) {
+            return \Response::json( $domain );
+        }
+        else {
+            return \Response::json( "none" );
+        }
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return bool
      */
-    public function update(Request $request, $id)
+    public function update(int $id)
     {
-        //
+        return $this->domainService->activate($id);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return bool
      */
-    public function destroy($id)
+    public function delete(int $id)
     {
-        //
+        return $this->domainService->deactivate($id);
     }
 }
